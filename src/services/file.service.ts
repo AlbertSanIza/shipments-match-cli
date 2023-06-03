05678487dd1b9b41ee1fb70fe45233da6d74daa9
Albert Sanchez <albert.san.iza@gmail.com>
2023-06-02 23:37:19 -0700

    Read Files (#3)
    * start file service
    
    * Check if file actually exists
    
    * Log if cant read file
    
    * Perform file check or finish execution
    
    * Update posible inputs
    
    * moved check inside readFile and parsed file
    
    * Remove extra step and continue if not empty

---
+++ src/services/file.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, Logger } from '@nestjs/common'
+import * as fs from 'fs'
+import * as path from 'path'
+
+@Injectable()
+export class FileService {
+    private readonly logger = new Logger(FileService.name)
+
+    readFile(filePath: string): string[] {
+        if (!this.fileExists(filePath)) {
+            return []
+        }
+
+        // Read the file and split into lines
+        let fileContent = fs.readFileSync(filePath, 'utf8').split(/\r?\n/)
+        // Remove any empty lines and trim edges
+        fileContent = fileContent.map((line) => line.trim()).filter((line) => line.length > 0)
+        // Some lines might contain multiple spaces, we want to replace them with a single space
+        fileContent = fileContent.map((line) => line.replace(/\s\s+/g, ' '))
+        // Some lines might have trailing commas, we want to remove them
+        fileContent = fileContent.map((line) => line.replace(/^,+/g, '').replace(/,+$/g, ''))
+        // Some lines might not have a space after a comma, we want to add it
+        fileContent = fileContent.map((line) => line.replace(/,(?!\s)/g, ', '))
+
+        return fileContent
+    }
+
+    private fileExists(filePath: string): boolean {
+        try {
+            // Replace any \ with /
+            // TODO: I need to confirm if this work in unix
+            const sanitizedFilePath = filePath.replace(/\\/g, '/')
+            // Resolve the path to the file
+            const resolvedPath = path.resolve(sanitizedFilePath)
+            // Check if the file exists, if it doesn't, an error will be thrown
+            const fileStats = fs.statSync(resolvedPath)
+            // Check if the file is a file and not a directory
+            if (!fileStats.isFile() || fileStats.isDirectory()) {
+                throw new Error()
+            }
+        } catch (error) {
+            this.logger.error(`"${filePath}" is not a valid file`)
+
+            return false
+        }
+
+        return true
+    }
+}
--- src/start.command.ts
+++ src/start.command.ts
@@ -1,5 +1,7 @@
 import { Command, CommandRunner } from 'nest-commander'
 
+import { FileService } from './services/file.service'
+
 @Command({
     name: 'start',
     arguments: '<destinations_file_path> <drivers_file_path>',
@@ -7,9 +9,25 @@ import { Command, CommandRunner } from 'nest-commander'
     options: { isDefault: true },
 })
 export class StartCommand extends CommandRunner {
+    constructor(private readonly fileService: FileService) {
+        super()
+    }
+
     async run(parameters: string[]): Promise<void> {
-        console.log('🚀 ~ StartCommand ~ run ~ parameters:', parameters)
+        // Grab input parameters
+        const [destinationsFilePath, driversFilePath] = parameters
+
         // Read files
+        const destinationsList = this.fileService.readFile(destinationsFilePath)
+        const driversList = this.fileService.readFile(driversFilePath)
+        // If the files are empty finish execution
+        if (!(destinationsList.length > 0 && driversList.length > 0)) {
+            return
+        }
+
+        console.log('🚀 ~ destinationsList:', destinationsList)
+        console.log('🚀 ~ driversList:', driversList)
+
         // Calculate all the suitability scores
         // Find the combination for maximum total suitability score
     }
