87832a618b55f661ce873b90b32b0840ea6c67f2
Albert Sanchez <albert.san.iza@gmail.com>
2023-06-02 23:33:29 -0700

    moved check inside readFile and parsed file

--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -6,7 +6,26 @@ import * as path from 'path'
 export class FileService {
     private readonly logger = new Logger(FileService.name)
 
-    fileExists(filePath: string): boolean {
+    readFile(filePath: string): string[] {
+        if (!this.fileExists(filePath)) {
+            return []
+        }
+
+        // Read the file and split into lines
+        let fileContent = fs.readFileSync(filePath, 'utf8').split(/\r?\n/)
+        // Remove any empty lines and trim edges
+        fileContent = fileContent.map((line) => line.trim()).filter((line) => line.length > 0)
+        // Some lines might contain multiple spaces, we want to replace them with a single space
+        fileContent = fileContent.map((line) => line.replace(/\s\s+/g, ' '))
+        // Some lines might have trailing commas, we want to remove them
+        fileContent = fileContent.map((line) => line.replace(/^,+/g, '').replace(/,+$/g, ''))
+        // Some lines might not have a space after a comma, we want to add it
+        fileContent = fileContent.map((line) => line.replace(/,(?!\s)/g, ', '))
+
+        return fileContent
+    }
+
+    private fileExists(filePath: string): boolean {
         try {
             // Replace any \ with /
             // TODO: I need to confirm if this work in unix
@@ -21,6 +40,7 @@ export class FileService {
             }
         } catch (error) {
             this.logger.error(`"${filePath}" is not a valid file`)
+
             return false
         }
 
