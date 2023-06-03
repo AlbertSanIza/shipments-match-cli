8873b7742c5bda82e885f8105d7862b02f98024a
Albert Sanchez <albert.san.iza@gmail.com>
2023-06-02 22:44:51 -0700

    Add arguments to commander run

--- src/start.command.ts
+++ src/start.command.ts
@@ -2,10 +2,15 @@ import { Command, CommandRunner } from 'nest-commander'
 
 @Command({
     name: 'start',
+    arguments: '<destinations_file_path> <drivers_file_path>',
+    description: 'Calculate Suitability Score for a list of Destinations and Drivers',
     options: { isDefault: true },
 })
 export class StartCommand extends CommandRunner {
     async run(parameters: string[]): Promise<void> {
         console.log('🚀 ~ StartCommand ~ run ~ parameters:', parameters)
+        // Read files
+        // Calculate all the suitability scores
+        // Find the combination for maximum total suitability score
     }
 }
