f55611b521e625cb8593c69cc0b6f6d858b68ab2
Albert Sanchez <albert.san.iza@gmail.com>
2023-06-02 23:12:36 -0700

    Check if file actually exists

--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common'
+import * as fs from 'fs'
+import * as path from 'path'
 
 @Injectable()
-export class FileService {}
+export class FileService {
+    fileExists(filePath: string): boolean {
+        try {
+            // Replace any \ with /
+            // TODO: I need to confirm if this work in unix
+            const sanitizedFilePath = filePath.replace(/\\/g, '/')
+            // Resolve the path to the file
+            const resolvedPath = path.resolve(sanitizedFilePath)
+            // Check if the file exists, if it doesn't, an error will be thrown
+            const fileStats = fs.statSync(resolvedPath)
+            // Check if the file is a file and not a directory
+            if (!fileStats.isFile() || fileStats.isDirectory()) {
+                throw new Error()
+            }
+        } catch (error) {
+            return false
+        }
+
+        return true
+    }
+}
--- src/start.command.ts
+++ src/start.command.ts
@@ -1,5 +1,7 @@
 import { Command, CommandRunner } from 'nest-commander'
 
+import { FileService } from './services/file.service'
+
 @Command({
     name: 'start',
     arguments: '<destinations_file_path> <drivers_file_path>',
@@ -7,8 +9,16 @@ import { Command, CommandRunner } from 'nest-commander'
     options: { isDefault: true },
 })
 export class StartCommand extends CommandRunner {
+    constructor(private readonly fileService: FileService) {
+        super()
+    }
+
     async run(parameters: string[]): Promise<void> {
-        console.log('🚀 ~ StartCommand ~ run ~ parameters:', parameters)
+        // Grab input parameters
+        const [destinationsFilePath, driversFilePath] = parameters
+
+        console.log(this.fileService.fileExists(destinationsFilePath))
+        console.log(this.fileService.fileExists(driversFilePath))
         // Read files
         // Calculate all the suitability scores
         // Find the combination for maximum total suitability score
