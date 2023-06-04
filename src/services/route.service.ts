c904abd95c23f871bb42b375d7dd3cffdd84f2d7
Albert Sanchez <albert.san.iza@gmail.com>
2023-06-03 17:15:09 -0700

    Calculate first if EVEN or ODD and update SS
    If length of destination is even
    SS is equal to the number of vowels in the driver's name multiplied by 1.5
    
    If length of destination is odd
    SS is equal to the number of consonants in the driver's name

--- src/services/route.service.ts
+++ src/services/route.service.ts
@@ -9,6 +9,20 @@ export class RouteService {
         if (!destination || !driver) {
             return suitabilityScore
         }
+
+        // If length of destination is even
+        if (destination.length % 2 === 0) {
+            // SS is equal to the number of vowels in the driver's name multiplied by 1.5
+            const vowelsCount = driver.match(/[aeiou]/gi).length
+            suitabilityScore = vowelsCount * 1.5
+        }
+        // If length of destination is odd
+        else {
+            // SS is equal to the number of consonants in the driver's name
+            // First we remove all non alphabetic characters
+            const consonantsCount = driver.replace(/[^a-zA-Z]/g, '').match(/[^aeiou]/gi).length
+            suitabilityScore = consonantsCount
+        }
         return suitabilityScore
     }
 }
