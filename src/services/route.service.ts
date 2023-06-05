07b2524c9c9212455e087f13e92a77671dfe0849
Albert Sanchez <albert.san.iza@gmail.com>
2023-06-05 04:23:56 -0700

    moved first algorithm to top of file

--- src/services/route.service.ts
+++ src/services/route.service.ts
@@ -4,6 +4,50 @@ import { BacktrackList, Route, Routes } from '../types/route'
 
 @Injectable()
 export class RouteService {
+    calculateRoutes(destinations: string[], drivers: string[]): Routes {
+        // Calculate all the suitability scores for all the possible combinations of drivers and destinations
+        const allPossibleRoutes: Route[] = []
+        for (const destination of destinations) {
+            for (const driver of drivers) {
+                allPossibleRoutes.push({
+                    destination: destination,
+                    driver: driver,
+                    suitabilityScore: this.calculateSuitabilityScore(destination, driver),
+                })
+            }
+        }
+
+        // Sort the results by Suitability Score in descending order
+        allPossibleRoutes.sort((a, b) => b.suitabilityScore - a.suitabilityScore)
+
+        // Dig recursively to take the best score, and filter out all items that contain the destination and name values of the best score
+        // We will apply this recursively until we have no more items in the list
+        const calculateBestRoutes = (routes: Route[]): Route[] => {
+            // If we have no more items in the list, return the list
+            if (routes.length === 0) {
+                return routes
+            }
+
+            // Grab first value from list
+            const bestRoute = routes[0]
+
+            // Filter out all items that contain the values of the best score
+            const remainingRoutes = routes.filter(
+                (route) => route.driver !== bestRoute.driver && route.destination !== bestRoute.destination,
+            )
+
+            return [bestRoute, ...calculateBestRoutes(remainingRoutes)]
+        }
+
+        const bestRoutes = calculateBestRoutes(allPossibleRoutes)
+        const totalSuitabilityScore = bestRoutes.reduce((acc, route) => acc + route.suitabilityScore, 0)
+
+        return {
+            list: bestRoutes,
+            suitabilityScore: totalSuitabilityScore,
+        }
+    }
+
     calculateRoutesV2(destinations: string[], drivers: string[]): Routes {
         // We are creating a matrix of suitability scores
         const suitabilityScoreMatrix: number[][] = []
@@ -75,50 +119,6 @@ export class RouteService {
         return {
             list: bestRoutes,
             suitabilityScore: highScore,
-        }
-    }
-
-    calculateRoutes(destinations: string[], drivers: string[]): Routes {
-        // Calculate all the suitability scores for all the possible combinations of drivers and destinations
-        const allPossibleRoutes: Route[] = []
-        for (const destination of destinations) {
-            for (const driver of drivers) {
-                allPossibleRoutes.push({
-                    destination: destination,
-                    driver: driver,
-                    suitabilityScore: this.calculateSuitabilityScore(destination, driver),
-                })
-            }
-        }
-
-        // Sort the results by Suitability Score in descending order
-        allPossibleRoutes.sort((a, b) => b.suitabilityScore - a.suitabilityScore)
-
-        // Dig recursively to take the best score, and filter out all items that contain the destination and name values of the best score
-        // We will apply this recursively until we have no more items in the list
-        const calculateBestRoutes = (routes: Route[]): Route[] => {
-            // If we have no more items in the list, return the list
-            if (routes.length === 0) {
-                return routes
-            }
-
-            // Grab first value from list
-            const bestRoute = routes[0]
-
-            // Filter out all items that contain the values of the best score
-            const remainingRoutes = routes.filter(
-                (route) => route.driver !== bestRoute.driver && route.destination !== bestRoute.destination,
-            )
-
-            return [bestRoute, ...calculateBestRoutes(remainingRoutes)]
-        }
-
-        const bestRoutes = calculateBestRoutes(allPossibleRoutes)
-        const totalSuitabilityScore = bestRoutes.reduce((acc, route) => acc + route.suitabilityScore, 0)
-
-        return {
-            list: bestRoutes,
-            suitabilityScore: totalSuitabilityScore,
         }
     }
 
